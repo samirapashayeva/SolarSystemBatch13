808926d205d40a5d1a4852ff8d02733733c40858
samirapashayeva <106215977+samirapashayeva@users.noreply.github.com>
2022-09-01 17:54:50 -0400

    Solar system ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        solarSystem inception= new solarSystem();
+        inception.Sun=1;
+        inception.Moon=4;
+        inception.Planets=2;
+
+        System.out.println("The basoc structure is ready");
+
+
+
+
+
     }
 }
